41b01826a81e5b6916de566570fabade4b341606
CarlosGomes00 <carlosfcgomes0@gmail.com>
2026-04-09 23:54:08 +0100

    [UPDATE] changed web search tools

--- tools.py
+++ tools.py
@@ -1,17 +1,22 @@
 import datetime
 import requests
 from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_tavily import TavilySearch
 from langchain_core.tools import tool
 import urllib.parse
 import os
 import subprocess
+from dotenv import load_dotenv, find_dotenv
 
 
-@tool('clock', description='Tool para obter dia e hora atual. Usa isto quando te perguntarem o dia ou a hora atual')
-def get_current_time(*args, **kwargs):
-    """Retorna a data e a hora atual. Útil para quando o utilizador perguntar que dia é hoje, ou que horas são."""
-    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#@tool('clock', description='Tool para obter dia e hora atual. Usa isto quando te perguntarem o dia ou a hora atual')
+#def get_current_time(*args, **kwargs):
+#    """Retorna a data e a hora atual. Útil para quando o utilizador perguntar que dia é hoje, ou que horas são."""
+#    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+
+load_dotenv(find_dotenv())
+chave_tavily = os.getenv("TAVILY_API_KEY")
 
 
 @tool('calculator', description='Tool para fazer calculos. Usa isto para qualquer problema matemático')
@@ -20,9 +25,17 @@ def calculator(expression : str):
     return str(eval(expression))
 
 
-web_search = DuckDuckGoSearchRun(
+#web_search = DuckDuckGoSearchRun(
     name="web_search",
     description="Usa isto para pesquisar na internet por informações atuais, notícias ou factos que desconheces. Sê explicito na pesquisa para obter a melhor informação possível"
+#)
+
+
+web_search = TavilySearch(
+    max_results=3,
+    search_depth="advanced", 
+    description="Usa isto para pesquisar na internet por informações atuais, notícias, resultados desportivos ou factos.",
+    tavily_api_key=chave_tavily
 )
 
 
@@ -36,8 +49,8 @@ def get_weather(localizacao : str):
         return f'Não foi possível obter o tempo: {e}'
     
 
-@tool('open_programs', description = 'Tool para abrir programas no computador. Apenas podes abrir os programas permitidos!!')
-def abrir_programa(nome_app: str):
+@tool('open_program', description = 'Tool para abrir programas no computador. Apenas podes abrir os programas permitidos!!')
+def open_program(nome_app: str):
     """Executa apenas aplicações permitidas usando subprocessos seguros"""
     
     apps_permitidas = {
@@ -48,10 +61,11 @@ def abrir_programa(nome_app: str):
             "calc": "calc.exe",
             "chrome": "chrome.exe",
             "browser": "chrome.exe",
-            "internet": "chrome.exe"
+            "internet": "chrome.exe",
+            "tradingview": "TradingView.exe"
     }
     
-    app_limpa = nome_app.lower()
+    app_limpa = nome_app.lower().replace(' ', '')
     if app_limpa in apps_permitidas:
         try:
             subprocess.run([apps_permitidas[app_limpa]], check=True, shell=False)
@@ -62,8 +76,8 @@ def abrir_programa(nome_app: str):
         return f"Acesso negado."
 
 
-@tool('play_music_spotify', description='Usa esta tool para abrir o Spotify e procurar por uma música, artista ou podcast específico.')
-def tocar_musica(pesquisa: str):
+@tool('play_spotify_music', description='Usa esta tool para abrir o Spotify e procurar por uma música, artista ou podcast específico.')
+def play_spotify_music(pesquisa: str):
     """Abre o Spotify com a pesquisa formatada."""
     try:
         query_formatada = urllib.parse.quote(pesquisa)
